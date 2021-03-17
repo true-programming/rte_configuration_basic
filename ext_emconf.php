fca32efcdd2b5c68faf475d64abc5240e63d06d4
Michael Semle <michael.semle@b13.com>
2021-03-17 20:44:07 +0100

    [TASK] Cleanup ext_emconf file

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,9 @@
 <?php
 
-/***************************************************************
- * Extension Manager/Repository config file for ext "rte_configuration_basic".
- ***************************************************************/
-
 $EM_CONF['rte_configuration_basic'] = [
     'title' => 'RTE basic onfiguration ',
     'description' => 'Adds a basic RTE configuration.',
     'category' => 'backend',
-    'shy' => 0,
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
@@ -17,14 +12,6 @@ $EM_CONF['rte_configuration_basic'] = [
         'conflicts' => [],
         'suggests' => [],
     ],
-    'loadOrder' => '',
-    'module' => '',
-    'state' => 'stable',
-    'uploadfolder' => 0,
-    'createDirs' => '',
-    'modify_tables' => '',
-    'clearcacheonload' => 0,
-    'lockType' => '',
     'author' => 'Michael Semle',
     'author_email' => 'info@mikeproduction.de',
     'author_company' => 'mikeproduction',
